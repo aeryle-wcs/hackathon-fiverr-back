46e0aa193797d4bd6a56136403fedf590e94e8d9
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-06-30 11:43:16 +0200

    Also remove passwords on getOne user

--- src/api/users/controllers/getOne.js
+++ src/api/users/controllers/getOne.js
@@ -4,7 +4,18 @@ module.exports = async (req, res, next) => {
   const { id } = req.params;
   try {
     const users = await prisma.user.findUnique({
-      where: { id },
+      where: {
+        id
+      },
+      select: {
+        id: true,
+        pseudo: true,
+        email: true,
+        status: true,
+        city: true,
+        job: true,
+        description: true,
+      }
     });
 
     res.status(200).json(users);
